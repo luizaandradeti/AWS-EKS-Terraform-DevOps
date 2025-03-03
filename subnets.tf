e66078fecebd4c05c6752aa86849ec3e59fa77c9
analuizaandr <analuizaandrade2025@icloud.com>
2025-03-03 18:10:59 -0300

    add subs

---
+++ subnets.tf
@@ -0,0 +1,49 @@
+resource "aws_subnet" "eks_subnet_private_1a" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags ={
+      tags =    {
+      Name= "${var.project_title}-priv-subnet-1a"
+    }
+  }
+}
+
+resource "aws_subnet" "eks_subnet_private_1b" {
+  vpc_id            = aws_vpc.eks_vpc.id
+   cidr_block        = "10.0.2.0/24"
+  availability_zone = "${data.aws_region.current.name}b"
+
+
+  tags ={
+      tags =    {
+      Name= "${var.project_title}-priv-subnet-1b"
+    }
+  }
+}
+
+resource "aws_subnet" "eks_subnet_public_1a" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "${data.aws_region.current.name}a"
+  map_public_ip_on_launch = true
+
+  tags ={
+      tags =    {
+      Name= "${var.project_title}-pub-subnet-1a"
+    }
+  }
+}
+
+resource "aws_subnet" "eks_subnet_public_1b" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "${data.aws_region.current.name}b"
+  map_public_ip_on_launch = true
+
+    tags =    {
+      Name= "${var.project_title}-pub-subnet-1b"
+    }
+  
+}
