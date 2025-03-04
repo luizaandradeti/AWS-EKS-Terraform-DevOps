1de0efc41ae896e37e5d3774b192c177b425efc5
Luiza Andrade <analuizaandrade2025@icloud.com>
2025-03-04 01:36:13 -0300

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -1,49 +1,50 @@
 resource "aws_subnet" "eks_subnet_private_1a" {
   vpc_id            = aws_vpc.eks_vpc.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.0.0/24"
   availability_zone = "${data.aws_region.current.name}a"
 
-  tags ={
-      tags =    {
-      Name= "${var.project_title}-priv-subnet-1a"
+  tags = {
+      Name= "${var.project_title}-priv-subnet-1a",
+      "kubernetes.io/role/internal-elb" = 1
     }
   }
-}
 
 resource "aws_subnet" "eks_subnet_private_1b" {
   vpc_id            = aws_vpc.eks_vpc.id
-   cidr_block        = "10.0.2.0/24"
+   cidr_block        = "10.0.1.0/24"
   availability_zone = "${data.aws_region.current.name}b"
 
 
-  tags ={
-      tags =    {
-      Name= "${var.project_title}-priv-subnet-1b"
+  tags = {
+      Name= "${var.project_title}-priv-subnet-1b",
+      "kubernetes.io/role/internal-elb" = 1
+    
     }
   }
-}
 
 resource "aws_subnet" "eks_subnet_public_1a" {
   vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = "10.0.3.0/24"
+  cidr_block              = "10.0.2.0/24"
   availability_zone       = "${data.aws_region.current.name}a"
   map_public_ip_on_launch = true
 
-  tags ={
-      tags =    {
-      Name= "${var.project_title}-pub-subnet-1a"
-    }
+  tags = {
+     Name= "${var.project_title}-pub-subnet-1a",
+     "kubernetes.io/role/elb" = 1
   }
 }
 
 resource "aws_subnet" "eks_subnet_public_1b" {
   vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = "10.0.4.0/24"
+  cidr_block              = "10.0.3.0/24"
   availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
 
-    tags =    {
-      Name= "${var.project_title}-pub-subnet-1b"
+    tags =  {
+      Name= "${var.project_title}-pub-subnet-1b", 
+      "kubernetes.io/role/elb" = 1
+
+
     }
   
 }
